65d280f2bdcc7719c7767b40d2bb06cc376d2890
GUILHERME DE OLIVEIRA CASTRO <202404492613@alunos.unifanor.edu.br>
2025-03-28 11:04:24 -0300

    Operadores de comparacao

---
+++ operadoresComparacao.py
@@ -0,0 +1,17 @@
+
+x = 10
+y = 10
+
+# Comparacao de igualdade
+print(x == y)
+
+# Diferenca
+print(x != y)
+
+# Superioridade (>)
+
+# Inferioridade (<)
+
+# Maior ou igual (>=)
+
+# Menor ou igual (<=)
