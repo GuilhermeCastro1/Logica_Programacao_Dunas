e0d8f6f0317a3d81332bcacd401a7156984d3f97
GUILHERME DE OLIVEIRA CASTRO <202404492613@alunos.unifanor.edu.br>
2025-03-28 10:04:39 -0300

    28/03

---
+++ operadoresAritmeticos.py
@@ -0,0 +1,36 @@
+# Adição
+numero1 = 10
+numero2 = 20
+
+soma = numero1 + numero2
+print (soma)
+
+# Subtração
+
+subtracao = numero1 - numero2
+print(subtracao)
+
+# Multiplicação
+
+multiplicacao = numero1 * numero2
+print(multiplicacao)
+
+# Divisão
+
+divisao = numero1 / numero2
+print(divisao)
+
+# Potência
+
+potencia = numero1 ** numero2
+print(potencia)
+
+# Módulo
+
+modulo = numero1 % numero2
+print(modulo)
+
+# Resto da divisão
+
+restodadivisao = numero1 // numero2
+print(restodadivisao)
