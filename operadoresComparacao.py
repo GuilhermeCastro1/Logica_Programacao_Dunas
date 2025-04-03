8710b315a05a8b9524c433f9c53a47ec40de872c
GuilhermeCastro1 <gc9874955@gmail.com>
2025-04-03 11:02:28 -0300

    Update operadoresComparacao.py

--- operadoresComparacao.py
+++ operadoresComparacao.py
@@ -9,9 +9,13 @@ print(x == y)
 print(x != y)
 
 # Superioridade (>)
+print(x > y)
 
 # Inferioridade (<)
+print(x < y)
 
 # Maior ou igual (>=)
+print( x >= y)
 
 # Menor ou igual (<=)
+print( x <= y)
